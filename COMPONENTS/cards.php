e64de7d3efb9a7505b933cf5dada33dbdd66b52d
waji27 <wajdanakmal37@gmail.com>
2023-09-06 16:24:23 +0500

    cards changed

--- COMPONENTS/cards.php
+++ COMPONENTS/cards.php
@@ -1,46 +1,96 @@
-<div class="cards-area">
-    <div class="wrapper">
-        <div class="card">
-            <h1>
-                <span class="enclosed">HTML</span>
-            </h1>
+<div class="container">
+    <div class="card">
+        <div class="face face1">
+            <div class="content">
+                <img
+                    src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true">
+                <h3>HTML</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Hyper Text Markup Language.</p>
+                <a href="#">Start Learning</a>
+            </div>
         </div>
     </div>
-    <div class="wrapper">
-        <div class="card">
-            <h1>
-                <span class="enclosed">CSS</span>
-            </h1>
+    <div class="card">
+        <div class="face face1">
+            <div class="content">
+                <img
+                    src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true">
+                <h3>CSS</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Used for Styling web pages.</p>
+                <a href="#">Start Learning</a>
+            </div>
         </div>
     </div>
-    <div class="wrapper">
-        <div class="card">
-            <h1>
-                <span class="enclosed">JavaScript</span>
-            </h1>
+    <div class="card">
+        <div class="face face1">
+            <div class="content">
+                <img
+                    src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true">
+                <h3>JavaScript</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Works like brain of website.</p>
+                <a href="#">Start Learning</a>
+            </div>
         </div>
     </div>
 </div>
-<div class="cards-area">
-    <div class="wrapper">
-        <div class="card">
-            <h1>
-                <span class="enclosed">PHP</span>
-            </h1>
+
+<div class="container">
+    <div class="card">
+        <div class="face face1">
+            <div class="content">
+                <img
+                    src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true">
+                <h3>PHP</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>A web server programming language.</p>
+                <a href="#">Start Learning</a>
+            </div>
         </div>
     </div>
-    <div class="wrapper">
-        <div class="card">
-            <h1>
-                <span class="enclosed">Bootstrape</span>
-            </h1>
+    <div class="card">
+        <div class="face face1">
+            <div class="content">
+                <img
+                    src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true">
+                <h3>Bootstrape</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Framework of CSS.</p>
+                <a href="#">Start Learning</a>
+            </div>
         </div>
     </div>
-    <div class="wrapper">
-        <div class="card">
-            <h1>
-                <span class="enclosed">React</span>
-            </h1>
+    <div class="card">
+        <div class="face face1">
+            <div class="content">
+                <img
+                    src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true">
+                <h3>React</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Framework of JavaScript.</p>
+                <a href="#">Start Learning</a>
+            </div>
         </div>
     </div>
+
 </div>
